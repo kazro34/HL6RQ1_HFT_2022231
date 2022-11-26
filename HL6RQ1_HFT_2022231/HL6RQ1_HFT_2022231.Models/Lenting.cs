a7b9f0c52124d9a1f463a1990456af5387bbbc00
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:30:05 +0100

    Created Lenting Class

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Models/Lenting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Models
+{
+    public class Lenting 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Out { get; set; }
+        public string In { get; set; }
+
+        [NotMapped]
+        public virtual Book LentBook { get; set; }
+
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+    }
+}
