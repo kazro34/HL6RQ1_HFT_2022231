a36ebf9175bf46f446e6905e54b04898490a7fdc
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:44:43 +0100

    Added LentingRepository

--- HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Repository/LentingRepository.cs
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Repository/LentingRepository.cs
@@ -7,21 +7,20 @@ using System.Threading.Tasks;
 
 namespace HL6RQ1_HFT_2022231.Repository
 {
-    public class AuthorRepository : Repository<Author>, IRepository<Author>
+    public class LentingRepository : Repository<Lenting>, IRepository<Lenting>
     {
-        public AuthorRepository(LibraryDbContext context) : base(context)
+        public LentingRepository(LibraryDbContext context) : base(context)
         {
 
         }
-        public override Author Read(int id)
+        public override Lenting Read(int id)
         {
-            return context.Authors.FirstOrDefault(t => t.authorId == id);
+            return context.Lentings.FirstOrDefault(t => t.Id == id);
         }
 
-
-        public override void Update(Author entity)
+        public override void Update(Lenting entity)
         {
-            var old = Read(entity.authorId);
+            var old = Read(entity.Id);
             foreach (var prop in old.GetType().GetProperties())
             {
                 prop.SetValue(old, prop.GetValue(entity));
