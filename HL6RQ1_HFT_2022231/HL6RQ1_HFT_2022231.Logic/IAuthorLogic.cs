319a911e87da5bf399f75efe37eb8ccb779a8847
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:48:48 +0100

    IAuthorLogic

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Logic/IAuthorLogic.cs
@@ -0,0 +1,18 @@
+﻿using HL6RQ1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Logic
+{
+    public interface IAuthorLogic
+    {
+        void Creat(Author item);
+        void Update(Author item);
+        void Delete(int id);
+        Author Read(int id);
+        IQueryable<Author> ReadAll();
+    }
+}
