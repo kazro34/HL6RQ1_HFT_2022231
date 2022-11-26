93389be777c6ef14d97f67836e4f60ead820a0df
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:37:04 +0100

    Added IRepository

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Update(T entity);
+        void Create(T Item);
+        void Delete(int id);
+    }
+}
