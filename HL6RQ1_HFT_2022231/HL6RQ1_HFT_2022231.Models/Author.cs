b364017f660e0795303a903507eead32fcb1cea8
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:28:56 +0100

    Created Author Class

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Models/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            this.Books = new HashSet<Book>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int authorId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
