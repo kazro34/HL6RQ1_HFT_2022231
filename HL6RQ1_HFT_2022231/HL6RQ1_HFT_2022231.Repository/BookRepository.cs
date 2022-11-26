d2511b5ae8192a23786fa939667ec3c6d16f43cd
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:39:21 +0100

    BookRepository Created

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Repository/BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using HL6RQ1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Repository
+{
+    public class BookRepository : Repository<Book>, IRepository<Book>
+    {
+        public BookRepository(LibraryDbContext context) : base(context)
+        {
+
+        }
+        public override Book Read(int id)
+        {
+            return context.Books.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Book entity)
+        {
+            var old = Read(entity.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(entity));
+            }
+            context.SaveChanges();
+        }
+    }
+}
