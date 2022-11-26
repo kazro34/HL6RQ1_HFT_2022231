6579e5a0b00266b6e798a04d831ee75061df93bf
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:46:31 +0100

    IBookLogic

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Logic/IBookLogic.cs
@@ -0,0 +1,20 @@
+﻿using HL6RQ1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Logic
+{
+    public interface IBookLogic
+    {
+        void Update(Book item);
+        void Delete(int id);
+        Book Read(int id);
+        void Create(Book item);
+        IQueryable<Book> ReadAll();
+        double AVGLentingPrice();
+        IEnumerable<KeyValuePair<string, double>> AVGLentingPricesByAuthors();
+    }
+}
