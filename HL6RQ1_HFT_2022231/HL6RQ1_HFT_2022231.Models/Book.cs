abf94539e642f3e408b20dbe5057cf870c5888dd
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:28:00 +0100

    Created Book Class

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Models/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int LentingFee { get; set; }
+
+        [NotMapped]
+        public virtual Author Author { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual ICollection<Lenting> Lentings { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
