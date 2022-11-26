13918903a48d9d1c56d8847378f9be265534ce9e
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 21:46:09 +0100

    Projects Created

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HL6RQ1_HFT_2022231.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HL6RQ1_HFT_2022231.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
