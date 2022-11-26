9569392cbe5f75f544a54bca44220585d2f77b14
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-26 22:50:42 +0100

    ILentingLogic

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Logic/ILentingLogic.cs
@@ -0,0 +1,21 @@
+﻿using HL6RQ1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.Logic
+{
+    public interface ILentingLogic
+    {
+        void Create(Lenting item);
+        void Update(Lenting item);
+        void Delete(int id);
+        Lenting Read(int id);
+        IQueryable<Lenting> ReadAll();
+        IEnumerable<KeyValuePair<string, double>> GetAverageIncomePerBookPerYear(int year);
+        IEnumerable<int> HasToPayFine();
+        IEnumerable<int> StillOpenLentsByBookId();
+    }
+}
