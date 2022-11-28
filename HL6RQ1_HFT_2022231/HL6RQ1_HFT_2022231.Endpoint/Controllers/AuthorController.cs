080359039f0b52b8f2bc0f52d7856182427ba7c5
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2022-11-28 17:52:36 +0100

    Author and Book Controllers

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.Endpoint/Controllers/AuthorController.cs
@@ -0,0 +1,44 @@
+﻿using HL6RQ1_HFT_2022231.Logic;
+using HL6RQ1_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HL6RQ1_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        IAuthorLogic logic;
+
+        public AuthorController(IAuthorLogic logic)
+        {
+            this.logic = logic;
+        }
+        [HttpGet]
+        public IEnumerable<Author> ReadAll()
+        { 
+            return this.logic.ReadAll();
+        }
+        [HttpGet("{id}")]
+        public Author Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+        [HttpPost]
+        public void Create([FromBody] Author value)
+        {
+            this.logic.Create(value);
+        }
+        [HttpPut]
+        public void Put([FromBody] Author value)
+        {
+            this.logic.Update(value);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
