3715fdcdd495d82650868ec3ca87da7dd6aa5456
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2023-02-22 14:07:35 +0100

    ViewModel (GUI Project Work)

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using HL6RQ1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.WPFClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Book> Books { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Books = new RestCollection<Book>("http://localhost:54941/", "book");
+        }
+    }
+}
