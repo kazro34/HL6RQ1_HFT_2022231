2c4f54189714eacefe9bdf213c460ca1a792206b
Kasza Róbert Gábor <kasza.robert@stud.uni-obuda.hu>
2023-02-22 16:34:08 +0100

    Create Author Command, Delete Author Command added (GUI Project Work)

--- HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.WPFClient/MainWindowViewModel.cs
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.WPFClient/MainWindowViewModel.cs
@@ -1,19 +1,56 @@
 ﻿using HL6RQ1_HFT_2022231.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HL6RQ1_HFT_2022231.WPFClient
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel: ObservableRecipient
     {
-        public RestCollection<Book> Books { get; set; }
+        public RestCollection<Author> Authors { get; set; }
+
+        private Author selectedAuthor;
+
+        public Author SelectedAuthor
+        {
+            get { return selectedAuthor; }
+            set {
+                     SetProperty(ref selectedAuthor, value);
+                     (DeleteAuthorCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+        }
+
+
+        public ICommand CreateAuthorCommand { get; set; }
+
+        public ICommand DeleteAuthorCommand { get; set; }
+
+        public ICommand UpdateAuthorCommand { get; set; }
 
         public MainWindowViewModel()
         {
-            Books = new RestCollection<Book>("http://localhost:54941/", "book");
+            Authors = new RestCollection<Author>("http://localhost:54941/", "author");
+
+            CreateAuthorCommand = new RelayCommand(() => {
+                Authors.Add(new Author()
+                {
+                    Name = "Kiss Béla"
+                });
+            });
+
+            DeleteAuthorCommand = new RelayCommand(() =>
+            {
+                Authors.Delete(SelectedAuthor.authorId);
+            },
+            () => 
+            {
+                return SelectedAuthor != null; 
+            }); 
         }
     }
 }
