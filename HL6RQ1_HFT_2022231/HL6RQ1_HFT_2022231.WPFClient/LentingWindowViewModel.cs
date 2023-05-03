3751ef9b92c4eac94df7ef8f20da21e696aeed3f
robert <kasza.robert@stud.uni-obuda.hu>
2023-05-03 15:50:56 +0200

    WindowViewModels added

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.WPFClient/LentingWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL6RQ1_HFT_2022231.WPFClient
+{
+    public class LentingWindowViewModel
+    {
+    }
+}
