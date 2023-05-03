24a1ed2c41d37d5555ff963389b7a138b12c5658
robert <kasza.robert@stud.uni-obuda.hu>
2023-05-03 15:25:01 +0200

    Main_Window added with menu buttons

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HL6RQ1_HFT_2022231.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Authors_Click(object sender, RoutedEventArgs e)
+        {
+            //AuthorWindow proc = new AuthorWindow();
+            //proc.Show();
+        }
+        private void Books_Click(object sender, RoutedEventArgs e)
+        {
+            //BookWindow proc = new BookWindow();
+            //proc.Show();
+        }
+        private void Lentings_Click(object sender, RoutedEventArgs e)
+        {
+            //LentingWindow proc = new LentingWindow();
+            //proc.Show();
+        }
+    }
+}
