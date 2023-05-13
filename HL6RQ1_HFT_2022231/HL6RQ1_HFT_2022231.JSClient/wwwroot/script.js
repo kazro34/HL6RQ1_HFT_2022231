d9669f52265fe47e4b9acbc120e96ae1ff921f3b
robert <kasza.robert@stud.uni-obuda.hu>
2023-05-13 18:58:58 +0200

    Delete added

--- HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.JSClient/wwwroot/script.js
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.JSClient/wwwroot/script.js
@@ -1,16 +1,55 @@
 ﻿let authors = [];
 
-fetch('http://localhost:54941/author')
-    .then(x => x.json())
-    .then(y => {
-        authors = y;
-        console.log(authors);     
-        display()
-    });
+getdata();
+
+
+
+async function getdata() {
+    await fetch('http://localhost:54941/author')
+        .then(x => x.json())
+        .then(y => {
+            authors = y;
+            console.log(authors);
+            display()
+        });
+}
+
+
 
 function display() {
+    document.getElementById('resultarea').innerHTML = "";
     authors.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
-            "<tr><td>" + t.authorId + "</td><td>" + t.name + "</td></tr>";
+            "<tr><td>" + t.authorId + "</td><td>" + t.name + "</td><td>" +
+        `<button type="button" onclick="remove(${t.authorId})">Delete</button>`
+            + "</td></tr>";
     });
+}
+
+function create() {
+    let name = document.getElementById('authorname').value;
+    fetch('http://localhost:54941/author/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Name: name })})
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
+
+function remove(id) {
+    fetch('http://localhost:54941/author/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
