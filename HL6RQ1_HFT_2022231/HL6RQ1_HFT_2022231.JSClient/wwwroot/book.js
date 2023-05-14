ade7fcee7ed0307ef1d81dc4b46bb7546f30b1c1
robert <kasza.robert@stud.uni-obuda.hu>
2023-05-14 12:16:10 +0200

    Book CRUDs added

---
+++ HL6RQ1_HFT_2022231/HL6RQ1_HFT_2022231.JSClient/wwwroot/book.js
@@ -0,0 +1,116 @@
+﻿let books = [];
+let connection;
+
+let bookIdToUpdate = -1;
+
+getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:54941/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("BookCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("BookDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.on("BookUpdated", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+}
+
+async function start() {
+    try {
+        awaitconnection.start();
+        console.log("SignalRConnected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+async function getdata() {
+    await fetch('http://localhost:54941/book/')
+        .then(x => x.json())
+        .then(y => {
+            books = y;
+            //console.log(books);
+            display()
+        });
+}
+
+
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    books.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>" + t.title + "</td><td>" +
+            `<button type="button" onclick="remove(${t.id})">Delete</button>` +
+            `<button type="button" onclick="showupdate(${t.id})">Update</button>`
+            + "</td></tr>";
+    });
+}
+
+function create() {
+    let title = document.getElementById('booktitle').value;
+    fetch('http://localhost:54941/book/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Title: title })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
+
+function showupdate(id) {
+    document.getElementById('booktitletoupdate').value = books.find(t => t['id'] == id)['title'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    bookIdToUpdate = id;
+}
+
+function update() {
+    let title = document.getElementById('booktitletoupdate').value;
+    fetch('http://localhost:54941/book', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Title: title, id: bookIdToUpdate })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
+
+function remove(id) {
+    fetch('http://localhost:54941/book/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
